1de4e829b879dfe6f6859de5429396f333ea4ce7
MEdahad <medahad16@gmail.com>
2020-04-16 03:08:24 +0300

    FP

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.PostGallery.as_view(), name='gallery_link'),
+    path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.views.generic.list import ListView
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all().order_by('-created_on')[:6]
+    queryset = Post.objects.all().order_by('-created_on')
     template_name = 'index.html'
+
+
+class PostGallery(ListView):
+    model = Post
+    queryset = Post.objects.order_by('-id')
+    template_name = 'gallery.html'
+    context_object_name = 'post_list'
+
+
+
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
